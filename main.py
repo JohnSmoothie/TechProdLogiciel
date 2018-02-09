136ab561540f384c9470f962ea75b7053efc889c
Victor BOIX <31928915+JohnSmoothie@users.noreply.github.com>
2018-02-09 14:26:02 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+
+tab = []
+with open('installation.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        tab.append(row)
+
+print(tab)
