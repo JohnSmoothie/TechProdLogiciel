3cfc24faea6d2da6144f0a0f792cfdc3a83ab226
Victor BOIX <31928915+JohnSmoothie@users.noreply.github.com>
2018-02-09 14:49:59 +0100

    t

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ tab2 = []
 with open('installation.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
-        for attribut in row:
-            tab2.append(attribut)
-        tab.append(tab2)
+        tab.append(row)
 
-print(tab)dddd
+print(tab[2])
+
+#utiliser sqlite pour les bd
