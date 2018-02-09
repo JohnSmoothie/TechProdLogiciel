751da6cb5040c567a6ff5d2e69e16afc461d23f4
Victor BOIX <31928915+JohnSmoothie@users.noreply.github.com>
2018-02-09 14:37:12 +0100

    plus de "
    plus de "

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 
 tab = []
 with open('installation.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
         tab.append(row)
 
