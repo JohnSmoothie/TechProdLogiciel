ac0c438a0c86d87d4def886bd146d5480c2d9edb
Victor BOIX <31928915+JohnSmoothie@users.noreply.github.com>
2018-02-09 14:41:48 +0100

    truc
    truc

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import csv
 
 tab = []
+tab2 = []
 with open('installation.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
-        tab.append(row)
+        for attribut in row:
+            tab2.append(attribut)
+        tab.append(tab2)
 
 print(tab)
